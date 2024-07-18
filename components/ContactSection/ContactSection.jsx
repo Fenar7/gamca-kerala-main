2aef23248ee1a57262f30709a0bf75a4661ae0ec
Fenar Khan <fenarkhan@gmail.com>
2024-07-18 20:15:17 +0530

    38

--- components/ContactSection/ContactSection.jsx
+++ components/ContactSection/ContactSection.jsx
@@ -5,28 +5,21 @@ function ContactSection() {
       <section className="contact-us-section container margin-bottom" id="contact">
       <div className="row contact-us">
         <div className="col-12 col-md-6 text-content d-flex flex-column justify-content-center">
-          <h5 className="contact-us-txt">Hey There</h5>
-          <h2 className="contact-us-txt">
+          <h4>Hey There</h4>
+          <h5>
             We'd Love To Hear From<br /><span>You</span>
-          </h2>
-          <p className="contact-us-txt">
-            Want to know more About our products?<br />
-            Or want to get a quote?<br />
-            Send us a message and we’ll respond as soon as possible
+          </h5>
+          <p>
+          Have Some Doubts?<br />
+          Send us a message and we’ll respond as soon as possible
           </p>
           <div className="contact-details d-flex flex-column">
             <div className="contact-item d-flex align-items-center contact-us-txt">
-              <img src="./images/location-icon.svg" alt="location icon" />
-              <p>Berad technology 
-                VJG8+32Q, AB Road, Akshayanagara West, Akshayanagar, Bengaluru, Karnataka 560076
-              </p>
-            </div>
-            <div className="contact-item d-flex align-items-center contact-us-txt">
-              <img src="./images/call-icon.png" alt="call icon" />
+              <img src="/call-icon-white.svg" alt="call icon" />
               <p>+91 9108552925</p>
             </div>
             <div className="contact-item d-flex align-items-center contact-us-txt">
-              <img src="./images/mail-icon.svg" alt="mail icon" />
+              <img src="/mail-icon-white.svg" alt="mail icon" />
               <p>hi@berad.in</p>
             </div>
           </div>
