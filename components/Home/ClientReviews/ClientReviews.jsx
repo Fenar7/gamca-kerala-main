a151470cdfb8afcf222fe8d279170ffcd4115351
Fenar Khan <fenarkhan@gmail.com>
2024-07-18 18:34:24 +0530

    36

--- components/Home/ClientReviews/ClientReviews.jsx
+++ components/Home/ClientReviews/ClientReviews.jsx
@@ -2,7 +2,7 @@ import './client.css'
 
 function ClientReviews() {
   return (
-    <section className="client-reviews-container-main">
+    <section className="client-reviews-container-main margin-bottom">
       <div className="client-reviews-container container">
         <div className="head d-flex align-items-center justify-content-between margin-bottom">
             <div className="heading">
@@ -22,9 +22,21 @@ function ClientReviews() {
             </div>
         </div>
 
-        <div className="review-cards-container">
-            <div className="cardd">
-                <img src="/user-icon" alt="" />
+        <div className="review-cards-container d-flex align-items-center justify-content-between">
+            <div className="cardd d-flex flex-column align-items-start justify-content-between">
+                <img src="/user-icon.svg" alt="" />
+                <h6>Sageesh Mm</h6>
+                <p>Staff is very caring and helpful especially the short guy ! Forgot his name ! Keep up the good work guys</p>
+            </div>
+
+            <div className="cardd d-flex flex-column align-items-start justify-content-between">
+                <img src="/user-icon.svg" alt="" />
+                <h6>Sageesh Mm</h6>
+                <p>Staff is very caring and helpful especially the short guy ! Forgot his name ! Keep up the good work guys</p>
+            </div>
+
+            <div className="cardd d-flex flex-column align-items-start justify-content-between">
+                <img src="/user-icon.svg" alt="" />
                 <h6>Sageesh Mm</h6>
                 <p>Staff is very caring and helpful especially the short guy ! Forgot his name ! Keep up the good work guys</p>
             </div>
