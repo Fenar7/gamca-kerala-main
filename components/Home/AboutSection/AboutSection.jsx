64483c917e6f9fa84604da5dc04f29b1927a66a2
Fenar Khan <fenarkhan@gmail.com>
2024-07-18 12:51:38 +0530

    23

--- components/Home/AboutSection/AboutSection.jsx
+++ components/Home/AboutSection/AboutSection.jsx
@@ -1,5 +1,7 @@
 import Button from '@/components/Button/Button'
 import React from 'react'
+import Image from 'next/image'
+import './about.css'
 
 function AboutSection({title, description}) {
   return (
@@ -11,8 +13,15 @@ function AboutSection({title, description}) {
                     <p>{description}</p>
                     <Button text="Book Appointment"/>
                 </div>
-                <div className="col-6 text-container">
-                    
+                <div className="col-6 image-container">
+                    <div className="image position-relative">
+                        <Image
+                        src="/about-us-image.png"
+                        layout="fill"
+                        objectFit="contain"
+                        alt="gamca kerala logo"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
