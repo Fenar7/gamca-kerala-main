7cdd24225a1779f3e989695a3996bcc7efa12c9f
Fenar Khan <fenarkhan@gmail.com>
2024-07-18 18:26:17 +0530

    35

--- components/Home/ClientReviews/ClientReviews.jsx
+++ components/Home/ClientReviews/ClientReviews.jsx
@@ -21,6 +21,14 @@ function ClientReviews() {
                     <img src="/ongoogle.svg" alt="" />
             </div>
         </div>
+
+        <div className="review-cards-container">
+            <div className="cardd">
+                <img src="/user-icon" alt="" />
+                <h6>Sageesh Mm</h6>
+                <p>Staff is very caring and helpful especially the short guy ! Forgot his name ! Keep up the good work guys</p>
+            </div>
+        </div>
       </div>
     </section>
   )
