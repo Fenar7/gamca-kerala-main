249636d4eedb01bafa0d0c59135fa99be0344634
Fenar Khan <fenarkhan@gmail.com>
2024-07-18 14:35:53 +0530

    30

--- components/Home/DocumentsRequired/DocumentsRequired.jsx
+++ components/Home/DocumentsRequired/DocumentsRequired.jsx
@@ -17,43 +17,44 @@ function DocumentsRequired() {
 
             <div className="item d-flex flex-column align-items-start">
                 <img src="/tickboard.svg" alt="" />
-                <h4>Saudi Arabia Medical</h4>
+                <h4>UAE medical</h4>
                 <p>
-                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5500 you have to pay at center for test. * Saudi Arabia Visa number, Visa date and Mofa number is required.
+                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5000 you have to pay at center for test.
                 </p>
             </div>
 
             <div className="item d-flex flex-column align-items-start">
                 <img src="/tickboard.svg" alt="" />
-                <h4>Saudi Arabia Medical</h4>
+                <h4>Bahrain Medical</h4>
                 <p>
-                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5500 you have to pay at center for test. * Saudi Arabia Visa number, Visa date and Mofa number is required.
+                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5000 you have to pay at center for test.
                 </p>
             </div>
 
             <div className="item d-flex flex-column align-items-start">
                 <img src="/tickboard.svg" alt="" />
-                <h4>Saudi Arabia Medical</h4>
+                <h4>Oman Medical</h4>
                 <p>
-                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5500 you have to pay at center for test. * Saudi Arabia Visa number, Visa date and Mofa number is required.
+                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos blue background. * Rs 5000 you have to pay at center for test.
                 </p>
             </div>
 
             <div className="item d-flex flex-column align-items-start">
                 <img src="/tickboard.svg" alt="" />
-                <h4>Saudi Arabia Medical</h4>
+                <h4>Qatar Medical</h4>
                 <p>
-                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5500 you have to pay at center for test. * Saudi Arabia Visa number, Visa date and Mofa number is required.
+                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5000 you have you to pay at center for test.
                 </p>
             </div>
 
             <div className="item d-flex flex-column align-items-start">
                 <img src="/tickboard.svg" alt="" />
-                <h4>Saudi Arabia Medical</h4>
+                <h4>Kuwait Medical</h4>
                 <p>
-                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Rs 5500 you have to pay at center for test. * Saudi Arabia Visa number, Visa date and Mofa number is required.
+                Rs 1500 Our fess for Gamca online Registration. Documents required: * Appointment slip * Original Passport + photocopy * 8 passport size photos white background. * Original visa is required. * Rs 5000 you have you to pay at center for test.
                 </p>
             </div>
+
         </div>
       </div>
     </section>
