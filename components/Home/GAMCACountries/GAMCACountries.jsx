8172963c44ff1f6258dd2925e33cb57806e80f28
Fenar Khan <fenarkhan@gmail.com>
2024-07-18 15:30:16 +0530

    32

--- components/Home/GAMCACountries/GAMCACountries.jsx
+++ components/Home/GAMCACountries/GAMCACountries.jsx
@@ -2,18 +2,71 @@ import './countries.css'
 
 function GAMCACountries() {
   return (
-    <section className="gamca-countries-container-main">
-      <div className="gamca-countries-container container d-flex align-items-center justify-content-center text-center">
+    <section className="gamca-countries-container-main margin-bottom">
+      <div className="gamca-countries-container container d-flex align-items-center justify-content-center text-center flex-column">
         <div className="heading-container d-flex align-items-center justify-content-center text-center">
             <h2>Apply for GAMCA Medical Tests in Your Country - Register Now!</h2>
         </div>
 
-        <div className="cards-container">
+        <div className="cards-container d-flex align-items-center justify-content-between">
             <div className="cardd">
                 <div className="image">
+                    <img src="/bahrain1.png" alt="" />
                     <h4>Bahrain</h4>
                 </div>
-                <button>Register Now</button>
+                <div className="button-container d-flex align-items-center justify-content-center">
+                    <button>Register Now</button>
+                </div>
+            </div>
+
+            <div className="cardd">
+                <div className="image">
+                    <img src="/bahrain1.png" alt="" />
+                    <h4>Bahrain</h4>
+                </div>
+                <div className="button-container d-flex align-items-center justify-content-center">
+                    <button>Register Now</button>
+                </div>
+            </div>
+
+            <div className="cardd">
+                <div className="image">
+                    <img src="/bahrain1.png" alt="" />
+                    <h4>Bahrain</h4>
+                </div>
+                <div className="button-container d-flex align-items-center justify-content-center">
+                    <button>Register Now</button>
+                </div>
+            </div>
+
+            <div className="cardd">
+                <div className="image">
+                    <img src="/bahrain1.png" alt="" />
+                    <h4>Bahrain</h4>
+                </div>
+                <div className="button-container d-flex align-items-center justify-content-center">
+                    <button>Register Now</button>
+                </div>
+            </div>
+
+            <div className="cardd">
+                <div className="image">
+                    <img src="/bahrain1.png" alt="" />
+                    <h4>Bahrain</h4>
+                </div>
+                <div className="button-container d-flex align-items-center justify-content-center">
+                    <button>Register Now</button>
+                </div>
+            </div>
+
+            <div className="cardd">
+                <div className="image">
+                    <img src="/bahrain1.png" alt="" />
+                    <h4>Bahrain</h4>
+                </div>
+                <div className="button-container d-flex align-items-center justify-content-center">
+                    <button>Register Now</button>
+                </div>
             </div>
         </div>
       </div>
