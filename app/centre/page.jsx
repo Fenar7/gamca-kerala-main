87bfa372d064af7515833fa27548806f36e96c5d
Fenar Khan <fenarkhan@gmail.com>
2024-07-20 13:12:37 +0530

    41

--- app/centre/page.jsx
+++ app/centre/page.jsx
@@ -1,11 +1,28 @@
 import SingleExamCenter from "@/components/SingleExamCenter/SingleExamCenter"
 
 function page() {
+    let centeres=[
+        {name:"This is name", address:"This is Address"},
+        {name:"This is name2", address:"This is Address2"},
+        {name:"This is name3", address:"This is Addres3"}
+    ]
+
+    
   return (
     <>
       <SingleExamCenter
       title="Convenient GAMCA Medical Centers in Ahmedabad: Book Your Appointment Now"
       placename="Ahmedabad"
+      secondtitle="GCC Medical centers in Ahmedabad"
+      paragraph="Has 3 (Three)
+        Discover GAMCA Approved Medical Centers, your gateway to seamless medical examinations for GCC visa candidates. Whether you're planning to travel to Bahrain, Kuwait, Saudi Arabia, Oman, Qatar, or the UAE, our centers are here to ensure your health clearance with ease. Simply schedule your appointment now and undergo your medical examination at a GAMCA Approved Medical Center nearest to you.
+
+        Starting April 1st, 2018, GAMCA (GCC) has transitioned to an online medical slip generator and payment system, offering candidates the convenience of generating their own medical slip and making payments online. Candidates can access the GAMCA website to generate their medical slip and complete their payment electronically. Alternatively, you can avail of our services for assistance with the process.
+
+        Please note that center assignments are automatic, and candidates cannot select their preferred center. Simply click on the 'book appointment' button, fill in the required details in the form according to your passport, select your city, and submit the form to initiate the process. Trust GAMCA Approved Medical Centers for a hassle-free experience in securing your health clearance for GCC travel."
+      image1="/ahmedabad1.png"
+      bgURL="/centre-hero-img.png"
+      centeres={centeres}
       />
     </>
   )
--- components/SingleExamCenter/SingleExamCenter.jsx
+++ components/SingleExamCenter/SingleExamCenter.jsx
@@ -1,14 +1,16 @@
 import './singlecenter.css'
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
-function SingleExamCenter({title,placename}) {
-  const heroContainerMainStyle = {
-    backgroundImage: `url(${backgroundImageUrl})`,
+function SingleExamCenter({title,placename, secondtitle, paragraph, image1, bgURL, centeres}) {
+  const bgStyle = {
+    backgroundImage: `url(${bgURL})`,
   };
 
 
   return (
     <section className="single-exam-centre-container-main">
-      <div className="hero-container-main">
+      <div className="hero-container-main" style={bgStyle}>
         <div className="hero-container container d-flex align-items-center justify-content-center flex-column text-center">
           <h1>{title}</h1>
           <div className="breadcrumb">
@@ -18,7 +20,43 @@ function SingleExamCenter({title,placename}) {
         </div>
       </div>
 
+      <div className="info-section-container-main margin-bottom">
+        <div className="info-section-container margin-top d-flex align-items-center justify-content-center">
+          <div className="row container">
+          <div className="col-12 col-md-6 text-container d-flex align-items-start justify-content-center flex-column">
+            <h2>{secondtitle}</h2>
+            <p>{paragraph}</p>
+            <Button text="Book an Appointment"/>
+          </div>
+          <div className="col-12 col-md-6 image-container d-flex flex-column align-items-end justify-content-between">
+            <div className="image position-relative">
+              <Image
+              src={image1}
+              layout="fill"
+              objectFit="cover"
+              alt="gamca kerala logo"
+              />
+            </div>
 
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="centeres-container-main container">
+        <div className="heading margin-bottom">
+          <h3>Gamca Approved Medical Examination Centers In {placename}</h3>
+        </div>
+
+        <div className="cards-container margin-bottom">
+        {centeres.map((centre, index) => (
+        <div className="cardd" key={index}>
+          <h4>{centre.name}</h4>
+          <p>{centre.address}</p>
+        </div>
+        ))}
+        </div>
+      </div>
     </section>
   )
 }
