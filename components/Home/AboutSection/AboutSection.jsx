95fd23c9e24ee9d95fd7ab4fdc62189f86a6c103
Fenar Khan <fenarkhan@gmail.com>
2024-07-21 06:58:38 +0530

    44

--- components/Home/AboutSection/AboutSection.jsx
+++ components/Home/AboutSection/AboutSection.jsx
@@ -8,12 +8,12 @@ function AboutSection({title, description}) {
     <section className="about-us-container-main">
         <div className="about-us-container container d-flex flex-column align-items-center justify-content-center">
             <div className="row">
-                <div className="col-6 text-container d-flex flex-column align-items-start justify-content-center">
+                <div className="col-12 col-md-6 text-container d-flex flex-column align-items-start justify-content-center">
                     <h2>{title}</h2>
                     <p>{description}</p>
                     <Button text="Book Appointment"/>
                 </div>
-                <div className="col-6 image-container d-flex align-items-center justify-content-center">
+                <div className="col-12 col-md-6 image-container d-flex align-items-center justify-content-center">
                     <div className="image position-relative">
                         <Image
                         src="/about-us-image.png"
