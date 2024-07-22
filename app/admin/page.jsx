6a0c03bdfb4dae58abd5eec3b486b1d29d4a366d
Fenar Khan <fenarkhan@gmail.com>
2024-07-22 20:37:09 +0530

    70

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import './admin.css'
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
@@ -31,26 +32,26 @@ export default function AdminLogin() {
   };
 
   return (
-    <div>
-      <h1>Admin Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <div className="container admin-login-container d-flex flex-column align-items-center justify-content-center margin-bottom margin-top">
+      <h1>Welcome Back Admin</h1>
+      <form onSubmit={handleSubmit} className="d-flex flex-column align-items-center justify-content-center">
+        <div className="row-main mg-top">
+          <div className="form-group custom-select-wrapper">
+            <label htmlFor="username">Username</label>
+            <div className="custom-select-container">
+              <input type="text" className="form-control custom-select" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </div>
+          </div>
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <div className="row-main mg-bottom">
+        <div className="form-group custom-select-wrapper">
+            <label htmlFor="password">Password</label>
+            <div className="custom-select-container">
+              <input type="password" className="form-control custom-select" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+          </div>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit userlogin-button">Login</button>
         {error && <p>{error}</p>}
       </form>
     </div>
